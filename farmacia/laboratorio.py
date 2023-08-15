2786d8ce12f7378b65fbf2b32c7ee6cd3614595c
Marco Binda <43681402+marcobinda@users.noreply.github.com>
2023-08-15 15:34:15 -0300

    Update laboratorio.py
    Definidos métodos cadastrar_novo e listar_laboratorio no módulo laboratorio.py

--- farmacia/laboratorio.py
+++ farmacia/laboratorio.py
@@ -1,5 +1,7 @@
 class Laboratorio:
 
+    dic_laboratorios = {}
+
     def __init__(self, nome: str, endereco: str, telefone: str, cidade: str, estado: str) -> None:
         self.nome = nome
         self.endereco = endereco
@@ -14,4 +16,20 @@ class Laboratorio:
         representacao += '\nCidade: ' + self.cidade
         representacao += '\nEstado: ' + self.estado
         return representacao
+    
+class CadastroLaboratorio:
+        
+    def cadastrar_novo(self) -> None:
+        nome = input("Digite o nome do laboratório: ")
+        endereco = input("Digite o endereço: ")
+        telefone = input("Digite o telefone: ")
+        cidade = input("Digite  o Município: ")
+        estado = input("Digite o Estado: ")
+
+        novo_lab = Laboratorio(nome, endereco, telefone, cidade, estado)
+        Laboratorio.dic_laboratorios[nome] = novo_lab
+
+    def listar_laboratorios(self):
+        return sorted(Laboratorio.dic_laboratorios.items())
+    
 
