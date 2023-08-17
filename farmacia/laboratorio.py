45a5edf211b997ead5617278cce3c33b0674d476
Marco Binda <43681402+marcobinda@users.noreply.github.com>
2023-08-16 22:18:58 -0300

    Update laboratorio.py

--- farmacia/laboratorio.py
+++ farmacia/laboratorio.py
@@ -3,6 +3,7 @@ class Laboratorio:
     dic_laboratorios = {}
 
     def __init__(self, nome: str, endereco: str, telefone: str, cidade: str, estado: str) -> None:
+
         self.nome = nome
         self.endereco = endereco
         self.telefone = telefone
@@ -10,8 +11,9 @@ class Laboratorio:
         self.estado = estado
 
     def __repr__(self) -> str:
-        representacao = 'Nome: ' + self.nome
-        representacao += '\Endereço: ' + self.endereco
+
+        representacao = '\nNome: ' + self.nome
+        representacao += '\nEndereço: ' + self.endereco
         representacao += '\nTelefone: ' + self.telefone
         representacao += '\nCidade: ' + self.cidade
         representacao += '\nEstado: ' + self.estado
@@ -20,16 +22,20 @@ class Laboratorio:
 class CadastroLaboratorio:
         
     def cadastrar_novo(self) -> None:
+
         nome = input("Digite o nome do laboratório: ")
         endereco = input("Digite o endereço: ")
         telefone = input("Digite o telefone: ")
-        cidade = input("Digite  o Município: ")
+        cidade = input("Digite o Município: ")
         estado = input("Digite o Estado: ")
 
         novo_lab = Laboratorio(nome, endereco, telefone, cidade, estado)
         Laboratorio.dic_laboratorios[nome] = novo_lab
 
     def listar_laboratorios(self):
-        return sorted(Laboratorio.dic_laboratorios.items())
+
+        sorted_dic = sorted(Laboratorio.dic_laboratorios.items(), key=lambda x:x[0])
+        for cadastro in sorted_dic:
+            print(cadastro)
     
 
