ec436c98ac802b32994588ba98b301a5a8cf23bb
Marco Binda <43681402+marcobinda@users.noreply.github.com>
2023-08-22 19:30:45 -0300

    Update medicamentos.py
    Update no módulo medicamentos.py para validação de campos e formação de impressão.

--- farmacia/medicamentos.py
+++ farmacia/medicamentos.py
@@ -10,10 +10,7 @@ class Medicamento:
         self.descricao = descricao
 
     def __repr__(self) -> str:
-        representacao = '\nNome: ' + self.nome
-        representacao += '\nPrincípio ativo: ' + self.comp_principal
-        representacao += '\nLaboratório: ' + self.laboratorio
-        representacao += '\nDescrição: ' + self.descricao
+        representacao = '\nNome: ' + self.nome + '\nPrincípio ativo: ' + self.comp_principal + '\nLaboratório: ' + self.laboratorio + '\nDescrição: ' + self.descricao
         return representacao
 class Fitoterapicos(Medicamento):
 
@@ -34,21 +31,27 @@ class CadastroMedicamento:
     def cadastrar_novo(self) -> None:
         nome = input("Digite o nome do medicamento: ")
         comp_principal = input('Digite o princípio ativo: ')
+
         laboratorio = input('Digite o nome do laboratório: ') 
         while (laboratorio not in Laboratorio.dic_laboratorios):
             print("Laboratório não cadastrado.")
             laboratorio = input('Digite o nome do laboratório: ') 
+
         descricao = input('Digite a descrição: ') 
+
         receita = input('O medicamento precisa de receita (S/s/N/n): ')
         while receita not in ['s', 'S', 'n', 'N']:
             print('Digite um valor válido.')
             receita = input('O medicamento precisa de receita (S/s/N/n): ')
+
         if (receita in ['s', 'S']):
             novo_medicamento = Quimioterapicos(nome, comp_principal, laboratorio, descricao, receita)
             Medicamento.dic_medicamentos[nome] = novo_medicamento
         else:
             novo_medicamento = Fitoterapicos(nome, comp_principal, laboratorio, descricao)
             Medicamento.dic_medicamentos[nome] = novo_medicamento
+
+        print(f'\n=== Medicamento cadastrado === {novo_medicamento}')
 
     def listar_medicamentos(self):
         sorted_dic = sorted(Medicamento.dic_medicamentos.values(), key=lambda med:med.nome)
