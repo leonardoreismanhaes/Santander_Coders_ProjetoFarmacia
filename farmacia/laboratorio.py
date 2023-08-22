f7b9f5c286079d0c467c93d2375c9c3841a2decc
Marco Binda <43681402+marcobinda@users.noreply.github.com>
2023-08-22 19:28:15 -0300

    Update laboratorio.py
    Update no módulo laboratorio.py para validação de campos e formação de impressão.

--- farmacia/laboratorio.py
+++ farmacia/laboratorio.py
@@ -1,3 +1,6 @@
+LEN_ESTADO = 2 # tamanho do campo estado
+LEN_TELEFONE = 10 # tamanho do campo telefone 
+
 class Laboratorio:
 
     dic_laboratorios = {}
@@ -11,12 +14,8 @@ class Laboratorio:
         self.estado = estado
 
     def __repr__(self) -> str:
-
-        representacao = '\nNome: ' + self.nome
-        representacao += '\nEndereço: ' + self.endereco
-        representacao += '\nTelefone: ' + self.telefone
-        representacao += '\nCidade: ' + self.cidade
-        representacao += '\nEstado: ' + self.estado
+        telefone_formatado = f'({self.telefone[:2]}) {self.telefone[2:6]}-{self.telefone[6:]}'
+        representacao = '\nNome: ' + self.nome + '\nEndereço: ' + self.endereco + '\nTelefone: ' + telefone_formatado + '\nCidade: ' + self.cidade + '\nEstado: ' + self.estado
         return representacao
     
 class CadastroLaboratorio:
@@ -25,12 +24,26 @@ class CadastroLaboratorio:
 
         nome = input("Digite o nome do laboratório: ")
         endereco = input("Digite o endereço: ")
-        telefone = input("Digite o telefone: ")
-        cidade = input("Digite o Município: ")
-        estado = input("Digite o Estado: ")
 
-        novo_lab = Laboratorio(nome, endereco, telefone, cidade, estado)
-        Laboratorio.dic_laboratorios[nome] = novo_lab
+        telefone = input("Digite o telefone: ")
+        while len(telefone) != LEN_TELEFONE:
+            print('O telefone deve conter 10 dígitos, sem pontuação.')
+            telefone = input("Digite o telefone: ")
+
+        cidade = input("Digite o Município: ")
+
+        estado = input("Digite o Estado: ")
+        while len(estado) != LEN_ESTADO:
+            print('A sigla da Unidade da Federação deve ter dois dígitos.')
+            estado = input("Digite o Estado: ")
+
+        if nome not in Laboratorio.dic_laboratorios:
+            novo_lab = Laboratorio(nome, endereco, telefone, cidade, estado)
+            Laboratorio.dic_laboratorios[nome] = novo_lab
+        else:
+            print('Laboratório já cadastrado.')
+
+        print(f'\n=== Laboratório cadastrado === {novo_lab}')
 
     def listar_laboratorios(self):
 
